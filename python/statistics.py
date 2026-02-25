6238851c0fbc5cb8a75ecb7d8697130f3ef399b6
Andrey Pershin <andrey_171717@mail.ru>
2026-02-26 02:00:05 +0700

    Немного обновил файлы

--- python/statistics.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def compute_zmap(pet_data, gm_mask):
-    values = pet_data[gm_mask]
-    mean = np.mean(values)
-    std = np.std(values)
-    z = np.zeros_like(pet_data)
-    z[gm_mask] = (pet_data[gm_mask] - mean) / std
-    return z
